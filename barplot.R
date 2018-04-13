69c8778e76755f2fa842d15bf94fa233157b9332
Alexander Struck <Alexander.Struck@hu-berlin.de>
2018-04-13 16:18:43 +0200

    replaced hard-coded values with vars
    mainly file names and directories, also max.capacity of storage cloud

--- barplot.R
+++ barplot.R
@@ -3,10 +3,14 @@
 # we have a cron job running that looks for media device 'cloud' and documents disk usage:
 # 11 11 * * * /bin/df -BG | grep 'media\/cloud' | awk '{print strftime("\%Y-\%m-\%d"), $3}' >> /path/to/cloud_storage_usage.txt
 ## declare vars input/output dir
-stats.location <- ""   # where is the usage stats file, needs the trailing slash
-output.dir <- stats.location
-# read file with specific name
-my.table <- read.table(paste0(stats.location, 'cloud_storage_usage.txt'))
+stats.file <- "cloud_storage_usage.txt"   # name of file that holds two columns of timestampt and disk usage
+stats.dir <- "/Users/lx/Desktop/"   # where is the usage stats file located, needs the trailing slash
+plot.name <- "BWGcloud_usage_plot_" # output file name, use trailing _ because timestamp is added
+output.dir <- stats.dir    # where should we write the PDF to
+max.capacity <- 5545    # our RAID0 currently offers this much storage capacity
+
+# read file with specified name
+my.table <- read.table(paste0(stats.dir, stats.file))
 my.table[,2] <- gsub('G$', '', my.table[,2]) # remove G behind 100G file space usage in column 2
 # we do have to deal with 1.1T & 1.3T & 1.4T due to vague counting by df -h in the past
 my.table[,2] <- gsub('1.1T', '1100', my.table[,2])
@@ -15,8 +19,8 @@ my.table[,2] <- gsub('1.4T', '1400', my.table[,2])
 my.table[,1] <- gsub('-\\d\\d$', '', my.table[,1]) # remove day notation from timestamp
 
 # open PDF device and start plotting
-pdf(paste0(stats.location, 'BWGcloud_usage_plot', Sys.Date(), '.pdf'), width=7, height=7, bg='transparent')
+pdf(paste0(output.dir, plot.name, Sys.Date(), '.pdf'), width=7, height=7, bg='transparent')
 barplot(as.numeric(my.table[,2]), names.arg=my.table[,1], col='chartreuse4', border='chartreuse4', ylim=c(0, 5555), las=1, main='BWGcloud file space usage')
-abline(h=5555, col="gray60") # 5545 1G blocks inG RAID0
-text(500, 5300, "max 5545 GB", col="gray60") # the y-coordinate should be set manually
+abline(h=max.capacity, col="gray60") # draws a horizontal line at value
+text(200, max.capacity-200, paste0("max ", max.capacity, " GB"), col="gray60") # places text below this line
 dev.off()
